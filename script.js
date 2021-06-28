fb28a0251e8d21ba65d500ed84bea1465b427822
Espeer5 <86035649+Espeer5@users.noreply.github.com>
2021-06-28 10:39:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function drawMenu() {
   var canvas = document.getElementById('menucan');
   var ctx = canvas.getContext('2d');
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  ctx.moveTo(1,1);
+  ctx.lineTo(9,1);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(1,5);
+  ctx.lineTo(9,5);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(1,9);
+  ctx.lineTo(9,9);
+  ctx.stroke();
