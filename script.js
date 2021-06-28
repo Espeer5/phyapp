4528e9305e681f92e9c0aabb97cf48264d3a1266
Espeer5 <86035649+Espeer5@users.noreply.github.com>
2021-06-28 11:19:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function drawMenu() {
   ctx.lineWidth = 1;
   ctx.beginPath();
   ctx.moveTo(0,1);
-  ctx.lineTo(15,1);
+  ctx.lineTo(20,1);
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(0,7);
-  ctx.lineTo(15,7);
+  ctx.lineTo(20,7);
   ctx.stroke();
   ctx.beginPath();
   ctx.moveTo(0,14);
-  ctx.lineTo(15,14);
+  ctx.lineTo(20,14);
   ctx.stroke();
 }
 
